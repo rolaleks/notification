2a2c8a1ea05bd25a6cde48054dbc9a3afe2e83ea
fz.pugachev <7197338-fz.pugachev@users.noreply.gitlab.com>
2020-10-25 09:04:19 +0200

    feat: доработан функционал бота. Добавлен функциона на строне бд для работы со стадиями бота, написан скрипт для генерации БД

---
+++ database-service/src/main/java/ru/geekbrains/model/ResponseMessage.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.model;
+
+import lombok.Data;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Data
+public class ResponseMessage {
+    String title;
+    String Details;
+    String date;
+
+    public ResponseMessage(String title, String details) {
+        this.title = title;
+        Details = details;
+        date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
+}
