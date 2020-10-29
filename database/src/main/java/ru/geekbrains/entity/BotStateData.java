a640ca5bb0be339a2b77511ecddadd9bbdd099a8
fz.pugachev <7197338-fz.pugachev@users.noreply.gitlab.com>
2020-10-29 22:52:01 +0200

    feat: рефакторинг

--- database/src/main/java/ru/geekbrains/entity/BotStateData.java
+++ database/src/main/java/ru/geekbrains/entity/BotStateData.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import ru.geekbrains.entity.user.User;
 
 import javax.persistence.*;
 
@@ -20,9 +21,18 @@ public class BotStateData {
     @Column(name = "id")
     Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
     @Column(name = "chat_id", unique = true)
-    String chatId;
+    long chatId;
 
     @Column(name = "state")
     int state;
+
+    public BotStateData(long chatId, int state) {
+        this.chatId = chatId;
+        this.state = state;
+    }
 }
